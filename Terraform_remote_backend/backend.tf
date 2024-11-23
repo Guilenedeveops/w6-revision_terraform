6942f7feee9753a37c5f7754f6e375b9c1188ef6
Guilenedeveops <Gpascale.fusillier@gmail.com>
2024-11-23 15:35:14 +0100

    without bucket name

--- Terraform_remote_backend/backend.tf
+++ Terraform_remote_backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "not-to-be-delete-bucket"
+    bucket = ""
     key    = "dev/terraform.tfsate"
     region = "us-east-1"
-    dynamodb_table = "terraform-lock-not-to-deleted"
+    dynamodb_table = ""
     encrypt = true
   }
 }
